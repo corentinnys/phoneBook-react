c3b8ac8917fb173f6362ea6c315101dfa43030a4
corentinnys <Skoloter23>
2023-07-13 09:55:21 +0200

    version-1 finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState(
+     ['']
+  ) 
+  const [newName, setNewName] = useState('')
+
+
+  function handleClick(e)
+  {
+    e.preventDefault()
+   
+    setPersons([...persons,newName]);
+  }
+  
+  function handleChange(e)
+  {
+    setNewName(e.currentTarget.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handleClick}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person=>person)}
+    </div>
   )
 }
 
